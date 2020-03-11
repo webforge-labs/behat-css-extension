a6dbb6d3d8478c3cfeb829a8b91c202b2abe3756
pscheit <p.scheit@ps-webforge.com>
2020-03-11 20:14:59 +0100

    cssExtension for Symfony 5.x

--- src/SymfonyCssExtension.php
+++ src/SymfonyCssExtension.php
@@ -1,11 +1,7 @@
 <?php
 namespace Webforge\Behat;
 
-use Symfony\Component\CssSelector\Exception\ExpressionErrorException;
-use Symfony\Component\CssSelector\Node\FunctionNode;
 use Symfony\Component\CssSelector\XPath\Extension\AbstractExtension;
-use Symfony\Component\CssSelector\XPath\Translator;
-use Symfony\Component\CssSelector\XPath\XPathExpr;
 
 class SymfonyCssExtension extends AbstractExtension
 {
@@ -20,7 +16,7 @@ class SymfonyCssExtension extends AbstractExtension
     /**
      * {@inheritdoc}
      */
-    public function getFunctionTranslators()
+    public function getFunctionTranslators(): array
     {
         return array(
             'has' => array($this, 'translateHas')
@@ -28,34 +24,11 @@ class SymfonyCssExtension extends AbstractExtension
     }
 
     /**
-     * @throws ExpressionErrorException
-     */
-    public function translateHas(XPathExpr $xpath, FunctionNode $function): XPathExpr
-    {
-        throw new \InvalidArgumentException('not yet');
-
-        $arguments = $function->getArguments();
-        foreach ($arguments as $token) {
-            if (!($token->isString() || $token->isIdentifier())) {
-                throw new ExpressionErrorException(
-                    'Expected a single string or identifier for :contains(), got '
-                    .implode(', ', $arguments)
-                );
-            }
-        }
-
-        return $xpath->addCondition(sprintf(
-            'contains(string(.), %s)',
-            Translator::getXpathLiteral($arguments[0]->getValue())
-        ));
-    }
-
-    /**
      * Returns extension name.
      *
      * @return string
      */
-    public function getName()
+    public function getName(): string
     {
         return CssElement::MINK_SELECTOR;
     }
